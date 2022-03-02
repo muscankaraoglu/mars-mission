a3b1a3555ff08c44832a417ea5211a627712c6a8
Mustafa Can Karaoğlu <muscankaraoglu@gmail.com>
2022-03-02 03:42:34 +0300

    Add project files.

---
+++ src/MarsMission/Models/Plateau.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsMission.Models
+{
+    public class Plateau
+    {
+        public Plateau(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ src/MarsMission/Models/Rover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsMission.Models
+{
+    public class Rover
+    {
+        public Rover(int x, int y, char direction,string instructions)
+        {
+            X = x;
+            Y = y;
+            Direction = Enum.Parse<Direction>(direction.ToString());
+            Instructions = instructions;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Direction Direction { get; set; }
+        public string Instructions { get; set; }
+    }
+}
